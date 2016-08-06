7efd100aa05cb3fb736017645f39712f56d2ba64
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-08-06 10:14:30 +0300

    Environments config support
    Different environments config support in the docpad.coffee

--- lib/typescript-plugin.ts
+++ lib/typescript-plugin.ts
@@ -1,12 +1,24 @@
+/// <reference path="../../typescript/lib/typescriptServices.d.ts" />
+
 var exportPlugin = require('./docpad-tsplugin');
 
+interface DocpadTypescriptOptions extends ts.CompilerOptions {
+    environments?: any
+}
+
+// ambient declaration to tell TypeScript what comes from Docpad parent class
+declare interface TypescriptPlugin {
+    getConfig(): DocpadTypescriptOptions
+} 
+
 class TypescriptPlugin {
-    static __super__: any
-    public config: any 
+    static __super__: any 
+    public docpad: any
+    private excludedStylesheets: any
 
     render(opts, next) : any {
         var config, inExtension, outExtension;
-        config = this.config;
+        config = this.getTsConfig();
         
         console.log(config)
 
@@ -19,9 +31,48 @@ class TypescriptPlugin {
         }
     }
     
+    /**
+     * extendCollection() is used to exclude from rendering files that have extension .ts but are not .js.ts files 
+     */
+    extendCollections(...opt) {
+        let config = this.getTsConfig()
+        let docpad = this.docpad
+
+        console.log('----------------------------------------------------------------------------------------------------')
+        console.log(config)
+        console.log('----------------------------------------------------------------------------------------------------')
+
+        /*
+         * If there is no source map spceified then no need to render *.ts files to Docpad's 'out' folder
+         */
+        if(config.inlineSourceMap !== true && config.sourceMap !== true ) {
+            this.excludedStylesheets = docpad.getDatabase().findAllLive({
+                filename: /^.*?[^\.][^j][^s]\.ts$/  // all files ending with .ts but not those ending with .js.ts 
+            })
+
+            return this.excludedStylesheets.on('add', function(model) {
+                return model.set({
+                    render: false,
+                    write: false
+                })
+            })
+        }
+    }
+
     constructor(...params) {
-	    console.log('Instanciated')
+	    TypescriptPlugin
+
+
         return TypescriptPlugin.__super__.constructor.apply(this, params);
+    }
+
+    /**
+     * Docpad has put the proper environment configuration only environments key muest be removed
+     */
+    getTsConfig() {
+        let config = this.getConfig()
+        delete config.environments
+        return config
     }
 }
 
