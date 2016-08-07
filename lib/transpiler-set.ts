1fc3519daa03860c8e699c063c10f3a7733e6f37
Todor Stefanov Kisov <todor.kisov@pracxs.com>
2016-08-07 23:17:05 +0300

    Almost finished

---
+++ lib/transpiler-set.ts
@@ -0,0 +1,50 @@
+import { TypescriptTranspiler, CompilerOptions, RootFile4Transpile, OutputFile } from "./typescript-transpiler"
+export { CompilerOptions, RootFile4Transpile, OutputFile, writeFile } from "./typescript-transpiler"
+
+/**
+ * This class holds set of transpilers based on list of different root directories.
+ * The list is specified at the very initialisation of the instance 
+ */
+export class TranspilerSet {
+    private transpilerInstances: TranspilerSet.TranspilerInstance[]
+
+    constructor(rootWorkingDirs: string[], private compilerOptions: CompilerOptions) {
+
+
+        this.transpilerInstances =
+            rootWorkingDirs.map( dir => { 
+                return {  
+                        rootWorkingDir: dir, 
+                        transpiler: new TypescriptTranspiler(dir, compilerOptions) 
+                    } 
+            })
+    }
+
+    /**
+     * Names of the files should be specified as full path to root to allow correct detection
+     * of their root working directory and that to enable uase of the right transpiler instance
+     */
+    public transpile(file: RootFile4Transpile): OutputFile[]
+    {
+        for( let trInstane of this.transpilerInstances )
+        {
+            // if has the same root as the root dir of cur transpiler
+            if( file.name.indexOf( trInstane.rootWorkingDir ) === 0 )
+            {
+                // to make Typescript transpiler to output to correct source maps paths
+                this.compilerOptions.mapRoot = trInstane.rootWorkingDir
+
+                return trInstane.transpiler.transpile( [ file ] )
+            }
+        }
+
+        throw (`Typescript Error: Failed to find root working directory for the file ${file.name}`)
+    }
+}
+
+namespace TranspilerSet {
+    export interface TranspilerInstance {
+        rootWorkingDir: string
+        transpiler: TypescriptTranspiler
+    }
+}
